f9b0cd0ed54a9767a6525575c11dafa7d2b8adeb
SisyphianLiger <69813075+SisyphianLiger@users.noreply.github.com>
2023-11-27 07:50:51 +0100

    Working on WebCrawler recursion

--- crawl.js
+++ crawl.js
@@ -64,20 +64,47 @@ function getURLsFromHTML(htmlBody, baseURL) {
     Crawling web pages 
 */
 
-async function crawlPage(baseURL) {
+async function crawlPage(baseURL, currentURL, pages) {
     try {
+    // 1. Make sure currentURL is on same domain as baseURL
+    if (!currentURL.includes(baseURL))
+        return pages
+
+    /*
+        3.  If the pages object already has an entry for the normalized version 
+            of the current URL, just increment the count and return the current 
+            pages.
+    
+    */
+            // 2. Get normalizeURL of currentURL
+        const norm_URL = normalizeURL(currentURL);
+    if (!pages[norm_URL] === undefined) {
+        count += 1;
+        return pages
+    } else 
+        pages[norm_URL] = response.text();
+
+
+    // check if currentURL is same domain as baseURL
     const response = await fetch(baseURL);
+
+
+
     // Checking HTTP Error
-    if (response.status >= 400)
-        return console.log(`Error: Response received a ${response.status}`)
+    if (response.status > 399) {
+        console.log(`Error: Response received a ${response.status}`);
+        return
+    }
     // Checking Proper Content-Type
-    if (!response.headers.get('Content-Type').includes('text/html')) 
-        return console.log(`Error: Not Response not text/html, format is ${response.headers.get('Content-Type')}`);
+    if (!response.headers.get('Content-Type').includes('text/html')) {
+        console.log(`Error: Not Response not text/html, format is ${response.headers.get('Content-Type')}`);
+        return
+    }
     // Return the Body Text
-    return await response.text();    
+    console.log(await response.text());
 
     } catch (err) {
-        console.log(err); 
+        console.log(err.message); 
     }
 }
 
